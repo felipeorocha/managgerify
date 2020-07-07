ba4f747d690b51d087a6225745aeeb258b3ec06b
Felipe Rocha <felipeo.rocha@hotmail.com>
2020-07-07 01:42:47 -0300

    Some API router tests(missing validations)

--- be/src/index.js
+++ be/src/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
+
 const app = express();
 
 // app.use(express.json());
 
-let people = [];
+const people = [];
 
 app.get('/', (req, res) => {
   return res.send(people);
+});
+
+app.get('/occurency', (req, res) => {
+  const { name } = req.query;
+
+  const results = name ? people.filter(item => item.name.includes(name)) : people;
+
+  return res.json(results);
 });
 
 app.get('/:id', (req, res) => {
@@ -36,9 +45,12 @@ app.put('/:id', (req, res) => {
 
 app.delete('/:id', (req, res) => {
   const { id } = req.params;
-  const person = people.filter(persona => persona.id !== id);
-  people = person;
-  return res.end('ok');
+  // const person = people.filter(persona => persona.id !== id);
+  // people = person; // people must be let
+  // return res.end('ok');
+  const personIndex = people.findIndex(person => person.id === id);
+  people.splice(personIndex, 1);
+  return res.status(204).end('ok');
 });
 
 app.listen(3000);
