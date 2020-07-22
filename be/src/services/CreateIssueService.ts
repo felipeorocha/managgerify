e4e442f67d3b2731aa8fd46ab454097e1babaddd
Felipe Rocha <felipeo.rocha@hotmail.com>
2020-07-22 02:27:44 -0300

    Add issues creator service

---
+++ be/src/services/CreateIssueService.ts
@@ -0,0 +1,31 @@
+import { startOfHour } from 'date-fns';
+import Issue from '../models/Issue';
+import IssuesRepository from '../repositories/IssuesRepository';
+
+interface Request {
+  owner: string;
+  message: string;
+  date: Date;
+}
+
+class CreateIssueService {
+  private issuesRepository: IssuesRepository;
+
+  constructor(issuesRepository: IssuesRepository){
+    this.issuesRepository = issuesRepository;
+  }
+
+  public execute({ owner, message, date }: Request): Issue {
+    const issueDate = startOfHour(date);
+
+    if (this.issuesRepository.findByDate(issueDate)) {
+      throw Error('Hour already booked');
+    }
+
+    const issue = this.issuesRepository.create({ owner, message, date: issueDate });
+
+    return issue;
+  }
+}
+
+export default CreateIssueService;
